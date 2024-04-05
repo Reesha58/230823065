7dc2447f50673b40f9293b2bce1396206f04daa1
Reesha58 <reeshamarsonia@gmail.com>
2024-04-05 08:10:42 +0530

    RESUME

---
+++ RESUME/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Resume from './Componets/Resume';
+
+
+function App() {
+  return (
+    <>
+    <Resume/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ RESUME/src/Componets/Resume.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Resume() {
+  return (
+    <div>
+      <div style={{ "backgroundColor": "lightblue", "padding": ".625rem" }}>
+
+        <h1> AMANDA TODO</h1>
+        <p>senior Product Manager</p>
+        <div style={{ "display": "flex" }}>
+          <h3 style={{ "marginLeft": "1.25rem" }}><svg style={{ "padding": "0rem", "margin": "-2 px" }} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3" /></svg>+1-857-791-5749</h3>
+          <h3 style={{ "marginLeft": "1.25rem" }}>@ elizabteh.w.todd@gmailo.com</h3>
+          <h3 style={{ "margin": "1.25rem" }}><svg style={{ "padding": "0rem", "margin": "-4 px" }} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M3.47 6.53a.75.75 0 0 1 1.06 1.061l-.727.727a2.743 2.743 0 0 0 3.879 3.879l.727-.727a.75.75 0 0 1 1.06 1.06l-.726.727a4.243 4.243 0 0 1-6-6zm8 1.879a.75.75 0 0 0 1.06 1.06l.727-.726a4.243 4.243 0 0 0-6-6l-.727.727a.75.75 0 0 0 1.061 1.06l.727-.727a2.743 2.743 0 0 1 3.879 3.879zm-.94-1.879a.75.75 0 1 0-1.06-1.06l-4 4a.75.75 0 1 0 1.06 1.06z" clip-rule="evenodd" /></svg>linkedin.com/in/elizabteh-W-todd</h3>
+        </div>
+
+      </div>
+      <div>
+        <h2 style={{"borderBottom":"5px solid black"}}>
+           SUMMARY
+        </h2>
+        <p>
+          After managing Products like the NA McDonald's  Mobile App, I decide that it is finally time to create my own business. With the help of 4 ex-colleagues, i founded S-HEalth, a Mobile app focused on women's health that was sold for $12.5M in 2020. Before i retrire , i am looking for a cause that i can contribute to with my product Management skills.
+        </p>
+        <div>
+          <h2 style={{"borderBottom":"5px solid black"}}>
+            EXPERIENCE
+          </h2>
+          <p>
+              Senior Product Manager
+              <p>S-HEalth</p>
+              <p>S-HEalth was a startup developing a new app that provides tips, advise,and  assistance on women's health that i established in 2017</p>
+             <p>.developed the go-to -market launch plan and product roadmap 
+             <p>.pitched the idea to investores in silicon valley</p>
+             <p>.Received and managed a $1.2 million project budget</p>
+             <p>.Led a team of 8 developeres and designers to create and manitain the app</p>
+             <p>increased paid customers from 50 to 4 500 in 2 months' time</p>
+             <p>sold the app to johnson & johnson for $12.5 million in 2020</p>
+             </p>
+          </p>
+        </div>
+    </div>
+ </div >
+  )
+}
