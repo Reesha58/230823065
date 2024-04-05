a912005ee5b1b15aa9bd39a0674e2a74c1dae41a
Reesha58 <reeshamarsonia@gmail.com>
2024-04-05 08:01:02 +0530

    React-Hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Useref1 from './Useref1.js';
 
-function App(props) {
+
+
+function App() {
   return (
-  <>
-  <div style={{"backgroundColor":`${props.color?props.color:"black"}`,"padding":"2px","marginTop":"10px"}}>
-    <p  style={{"color":"white","fontsize":"10px"}}>{props.title}</p>
-  </div>
-  <div style={{"backgroundColor":"grey","padding":"10px"}}>
-    <p  style={{"color":"white","margin":"0px","fontsize":"10px","padding":"0px"}}>{props.content}</p>
-  </div>
-  </>
+
+    <div style={{ "margin": "30px" }}>
+      <Useref1 />
+      {/* <Input /> */}
+    </div>
   );
 }
 
